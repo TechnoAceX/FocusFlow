790896442eb2d4743e7a0bfe5dfd1dff59740356
Pawan Manghnani <pawanmanghnani26@gmail.com>
2026-04-20 19:10:24 +0530

    Added ui and a new look

--- focusflow/core/views.py
+++ focusflow/core/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from .models import StudySession
 
-# Create your views here.
+def add_session(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        StudySession.objects.create(
+            user=request.user,
+            subject=data['subject'],
+            hours=data['duration'] / 60
+        )
+
+        return JsonResponse({"status": "success"})
+    
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+from django.http import JsonResponse
+from .models import StudySession
+
+def reset_sessions(request):
+    if request.method == "POST":
+        StudySession.objects.all().delete()
+        return JsonResponse({"status": "cleared"})
