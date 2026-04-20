2119b441574cdbe6d11fae2d34b07efe5b7879fd
Pawan Manghnani <pawanmanghnani26@gmail.com>
2026-04-21 01:51:15 +0530

    clean wsgi fix

--- focusflow/focusflow/settings.py
+++ focusflow/focusflow/settings.py
@@ -32,7 +32,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # 🚀 APPS
 INSTALLED_APPS = [
-    'core',
+    'focusflow.core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
