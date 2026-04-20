f491a408358aa45853299ca52b313c06d5eec5e0
Pawan Manghnani <pawanmanghnani26@gmail.com>
2026-04-21 02:23:51 +0530

    fix app import path

--- focusflow/focusflow/wsgi.py
+++ focusflow/focusflow/wsgi.py
@@ -1,10 +1,9 @@
 import os
-import sys   # ✅ ADD THIS
-
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'focusflow.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'focusflow.focusflow.settings'   # ✅ THIS IS THE FIX
+)
 
 application = get_wsgi_application()
