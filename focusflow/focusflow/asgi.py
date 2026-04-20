93b4e394821937056cd1a9e56ee1169417a89f40
Pawan Manghnani <pawanmanghnani26@gmail.com>
2026-04-21 00:47:28 +0530

    fix settings module path

--- focusflow/focusflow/asgi.py
+++ focusflow/focusflow/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'focusflow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'focusflow.focusflow.settings')
 
 application = get_asgi_application()
